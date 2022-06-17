a3bf5e310ca893e010a1c7cd2601499c323cdc35
Arno Burkhart <arburk@users.noreply.github.com>
2022-06-17 10:14:23 +0200

    Additional playing with classes and adding dashboard

--- src/main/java/com/github/arburk/jobrunr/Demo.java
+++ src/main/java/com/github/arburk/jobrunr/Demo.java
@@ -1,32 +1,65 @@
 package com.github.arburk.jobrunr;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
 import org.jobrunr.configuration.JobRunr;
 import org.jobrunr.scheduling.BackgroundJob;
-import org.jobrunr.scheduling.cron.Cron;
 import org.jobrunr.storage.InMemoryStorageProvider;
-
-import java.util.Date;
 
 public class Demo {
 
+  public static final String CRON = "*/5 * * * * *"; //every 5 seconds
+  private static final String ID = "myJobId";
+  public static final String FILE_NAME = "C:\\dev\\gitRepos\\_github\\jobrunr-io\\joblog.txt";
+  private static InMemoryStorageProvider storageProvider;
+
   public static void main(String[] args) {
-    InMemoryStorageProvider storageProvider = new InMemoryStorageProvider();
+
+    storageProvider = new InMemoryStorageProvider();
     JobRunr.configure()
         .useStorageProvider(storageProvider)
         .useBackgroundJobServer()
+        .useDashboard()
         .initialize();
 
-    System.out.println("programm started");
+    FileLogger.fileName = FILE_NAME;
+    System.out.println("programm started at " + new Date());
+    new Demo().run();
+  }
 
-    BackgroundJob.enqueue(
-        () -> System.out.println("Hello")
-    );
+  private void run() {
+    BackgroundJob.scheduleRecurrently(ID, CRON, () -> FileLogger.run());
+    System.out.println("myJob was scheduled with " + CRON);
+    storageProvider.getRecurringJobs().forEach(System.out::println);
+    System.out.println("use http://localhost:8000/dashboard/ to view jobs");
+  }
 
-    BackgroundJob.scheduleRecurrently(
-        Cron.minutely(),
-        () -> System.out.println("executed " + new Date())
-    );
 
-    System.out.println("programm stopped");
+  public static class FileLogger {
+
+    private static String fileName;
+
+    public FileLogger(String fileName) {
+      FileLogger.fileName = fileName;
+      run("Created at " + new Date());
+    }
+
+    public static void run() {
+      System.out.println("ping...");
+      run("I was executed at " + new Date());
+    }
+
+    private static void run(String text) {
+      try (FileWriter fileWriter = new FileWriter(fileName, true);
+           PrintWriter printWriter = new PrintWriter(fileWriter)) {
+        printWriter.println(text);
+        printWriter.flush();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
   }
 }
